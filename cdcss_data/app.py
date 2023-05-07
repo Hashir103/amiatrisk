9fc1147e8cce30510b840d2d482889207b1da9f6
Hashir <50779880+Hashir103@users.noreply.github.com>
2023-05-07 02:44:00 -0400

    added chatbot to diseases page

--- cdcss_data/app.py
+++ cdcss_data/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for
 import cdcss_top_five_finder as cdcss
-
+import json
+import cohere
+co= cohere.Client('AnhwcBAs6bP9NTeUYvAfsI0eMNYTiykmbSVg8HKg')
 app = Flask(__name__)
 @app.route("/")
 def home():
@@ -26,9 +28,42 @@ def question():
 #         race = request.form['race']
 #         fam = request.form['fam']
 
-@app.route("/diseases")
-def diseases():
+@app.route('/diseases')
+def diseasePage():
     return render_template("diseases.html")
+
+@app.route("/", methods=["POST"])
+def diseases():
+    output = request.get_json()
+    result = json.loads(output)
+    getResponse(result)
+    txt1 = "{response}".format(response=result)
+    return render_template("diseases.html")
+
+@app.route("/test", methods=["POST", "GET"])
+def getResponse():
+  # response = ''
+  input_json = ''
+  content_type = request.headers.get('Content-Type')
+  if (content_type == 'application/json'):
+      input_json = request.json
+      # return json
+  else:
+      return 'Content-Type not supported!'
+  inputText = json.loads(json.dumps(input_json))["userInput"]
+
+  if request.method == "GET":
+    return "hello"
+  if request.method == "POST":
+    response = co.generate(
+      model='command-xlarge-nightly',
+      prompt= inputText,
+      max_tokens=900,
+      temperature=0.5,
+      k=0,
+      stop_sequences=[],
+      return_likelihoods='GENERATION')
+  return response.generations[0].text
         
 @app.route("/applynow")
 def applynow():
